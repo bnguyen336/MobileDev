ab5db64a2748dd1ef831b174a8149ab6df226f68
bnguyen336 <binhnguyen1092@gmail.com>
2016-09-01 16:29:44 -0700

    09-01

--- app/src/main/java/edu/orangecoastcollege/cs273/bnguyen336/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/bnguyen336/yarrworld/MainActivity.java
@@ -1,13 +1,23 @@
 package edu.orangecoastcollege.cs273.bnguyen336.yarrworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Define the button as an instance variable (number)
+    Button speakButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Restoring user settings from previous session
         super.onCreate(savedInstanceState);
+        //Initiating (!) opening the layout called activity_main(.xml)
         setContentView(R.layout.activity_main);
+
+        //Hook up speakButton to the one in our view
+        speakButton = (Button) findViewById(R.id.speakButton);
     }
 }
