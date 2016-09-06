0a3079fbae246fe7e5f74d569cd9eb9ec7f9c2de
bnguyen336 <binhnguyen1092@gmail.com>
2016-09-06 14:33:49 -0700

    09/06

--- app/src/main/java/edu/orangecoastcollege/cs273/bnguyen336/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/bnguyen336/yarrworld/MainActivity.java
@@ -2,7 +2,9 @@ package edu.orangecoastcollege.cs273.bnguyen336.yarrworld;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
         //Hook up speakButton to the one in our view
         speakButton = (Button) findViewById(R.id.speakButton);
+        speakButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toast_text, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
